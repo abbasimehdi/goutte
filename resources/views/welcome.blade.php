41dfc1ff1bcd17ee237d7a9df964438f7dc77ff2
Abbasi Mehdi <abbasimhdi@gmail.com>
2023-11-28 15:23:16 +0330

    Add product form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,19 @@
     <title>Admin Panel</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <style>
+        .mystyle {
+           display: none;
+        }
+    </style>
 </head>
 <body style="background: black; color: aliceblue">
 
 <div class="container">
-    <div class="col-md-3 mx-auto">
+    <button class="btn btn-success" onclick="myFunction()">create product</button>
+    <div class="col-md-3 mx-auto"  id="myDIV">
         <h2>Product Form</h2>
     <form
         action=""
@@ -68,7 +73,7 @@
                 </tr>
             @else
                 <tr>
-                    <td colspan="10">
+                    <td class="text-center">
                         <p class="text-center text-danger">{{ __('Result not found') }}</p>
                     </td>
                 </tr>
@@ -76,5 +81,12 @@
         </tbody>
     </table>
 </div>
+
+<script>
+    function myFunction() {
+        var element = document.getElementById("myDIV");
+        element.classList.toggle("mystyle");
+    }
+</script>
 </body>
 </html>
