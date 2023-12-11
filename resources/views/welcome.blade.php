26d377612dc93ce9188559f573b836032fd66a18
Abbasi Mehdi <abbasimhdi@gmail.com>
2023-12-11 13:40:36 +0330

    Get sample product details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,9 +16,9 @@
         <tr>
             <th>order</th>
             <th>title</th>
+            <th>price</th>
             <th>image</th>
-            <th>like</th>
-            <th>created at</th>
+            <th>original site url</th>
             <th>operation</th>
         </tr>
         </thead>
@@ -28,25 +28,19 @@
             @foreach($products as $product)
                 <tr>
                     <td>{{ $i }}</td>
-                    <td>{{ $product->title }}</td>
+                    <td>{{ $product['header']?? 'title' }}</td>
+                    <td>{{ $product['price']?? 0 }}</td>
                     <td>
-                        <img
-                            src='{{ asset($product->image) }}'
-                            width="50" height="50" alt="image">
+                        @if($productImages)
+                            @foreach($productImages as $productImage)
+                                <img
+                                    src='{{ asset($productImage ?? "https://static.nike.com/a/images/w_1536,c_limit/22f6b7ae-1c09-4aa1-a8f0-3e54a8bec589/image.jp") }}'
+                                    width="50" height="50" alt="image"
+                                >
+                            @endforeach
+                        @endif
                     </td>
-                    <td>
-                        <form action="{{ url('api/v1/main/product/like') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="user_id" value="1">
-                            <input type="hidden" name="product_id" value="{{ $product->id }}">
-                            <button type="submit" class="btn btn-default">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
-                                    <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-                                </svg>
-                            </button>
-                        </form>
-                    </td>
-                    <td>{{ $product->created_at }}</td>
+                    <td><a href="{{ $product['original-site-url'] }}" _blank>go to sute</a></td>
                     <td>
                         <button class="btn btn-warning">edit</button>
                         <button class="btn btn-danger">remove</button>
@@ -56,7 +50,7 @@
             @endforeach
             <tr>
                 <td colspan="6">
-                    {{ $products->links() }}
+{{--                    {{ $products->links() }}--}}
                 </td>
             </tr>
         @else
